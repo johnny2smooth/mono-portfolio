b4e9917ba914312cf44cb0bc109d6e266791f9b9
johnny2smooth <johnbp13@gmail.com>
2022-12-21 19:54:59 -0800

    photos separated into own component. figuring out how js works in Astro bit by bit

--- src/scripts/bionicConverter.ts
+++ src/scripts/bionicConverter.ts
@@ -1,15 +1,21 @@
-export default function bionicConverter(bodyTextContent: string) {
-  const eachWord = bodyTextContent.split(" ");
-  const bionicWord = eachWord?.map((word) => {
-    let boldPart;
-    if (word.length % 2 === 0) {
-      boldPart = word.slice(0, Math.floor(word.length / 2));
-    } else {
-      boldPart = word.slice(0, Math.floor((word.length + 1) / 2));
-    }
-    return `<span class="font-bold">${boldPart}</span>${word.slice(boldPart.length)}`;
+export default function bionicConverter() {
+  const bionic = document.querySelectorAll('.bionic');
+  bionic.forEach((paragraph) => {
+    addEventListener('click', () => {
+      const eachWord = (paragraph as HTMLElement).innerText.split(' ');
+      const bionicWord = eachWord?.map((word) => {
+        let boldPart;
+        if (word.length % 2 === 0) {
+          boldPart = word.slice(0, Math.floor(word.length / 2));
+        } else {
+          boldPart = word.slice(0, Math.floor((word.length + 1) / 2));
+        }
+        return `<span class="font-semibold bionic">${boldPart}</span>${word.slice(
+          boldPart.length
+        )}`;
+      });
+      paragraph.innerHTML = bionicWord?.join(' ');
+    });
   });
-  return bionicWord?.join(" ");
-}
 
-console.log(bionicConverter("Hello World I think that the universe is a simulation"));
+}
